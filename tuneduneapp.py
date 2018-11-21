be27b2d3370fe6396f05097202906bef10fbc0fc
rodetroner <rodetroner@gmail.com>
2018-11-22 00:09:30 +0100

    Add empty window

--- tuneduneapp.py
+++ tuneduneapp.py
@@ -3,10 +3,14 @@ kivy.require('1.10.1')
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
-class TuneDune(App):
+class MyWindow(BoxLayout):
+    pass
+
+class TuneDuneApp(App):
     def build(self):
-        return Label(text='Hello world')
+        return MyWindow()
 
 if __name__ == '__main__':
-    TuneDune().run()
+    TuneDuneApp().run()
