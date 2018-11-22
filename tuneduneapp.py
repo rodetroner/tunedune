d35edd9e2f7aea9ab55d9dcac4cdc1f6eac30a6e
rodetroner <rodetroner@gmail.com>
2018-11-22 02:03:03 +0100

    Change layout to Stack and fix button sizes

--- tuneduneapp.py
+++ tuneduneapp.py
@@ -3,9 +3,9 @@ kivy.require('1.10.1')
 
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.stacklayout import StackLayout
 
-class MyWindow(BoxLayout):
+class MyWindow(StackLayout):
     pass
 
 class TuneDuneApp(App):
