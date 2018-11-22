725735cc58db779ac0677e48bc014774d112986c
rodetroner <rodetroner@gmail.com>
2018-11-22 02:26:07 +0100

    Import button element

--- tuneduneapp.py
+++ tuneduneapp.py
@@ -3,6 +3,7 @@ kivy.require('1.10.1')
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.uix.stacklayout import StackLayout
 
 class MyWindow(StackLayout):
