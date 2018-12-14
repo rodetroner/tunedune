1fd24a766c0760a5d6df43945979d86cd8b2c43a
rodetroner <rodetroner@gmail.com>
2018-12-14 15:01:30 +0100

    Change UI layout
    Change base layout to AnchorLayout so that there is space for
    developing right side of the window, where lists of tracks will be
    displayed.
    
    It's possible that it's still a wrong way to go and maybe Grid Layout
    is the proper one.

--- tunedune.py
+++ tunedune.py
@@ -5,13 +5,14 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.anchorlayout import AnchorLayout
 
-class MyWindow(StackLayout):
+class MainLayout(AnchorLayout):
     pass
 
 class TuneDuneApp(App):
     def build(self):
-        return MyWindow()
+        return MainLayout()
 
 if __name__ == '__main__':
     TuneDuneApp().run()
