c4e284648fc13fad148d4e7ae532ef1d17f46ec6
Szymon Hajnos <rodetroner@gmail.com>
2019-01-04 15:17:19 +0100

    Commit the unstaged files that should be commited last time

--- tunedune.py
+++ tunedune.py
@@ -2,10 +2,11 @@ import kivy
 kivy.require('1.10.1')
 
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.stacklayout import StackLayout
+from kivy.lang import Builder
 from kivy.uix.anchorlayout import AnchorLayout
+
+Builder.load_file('sidebar.kv')
+Builder.load_file('tracklist.kv')
 
 class MainLayout(AnchorLayout):
     pass
