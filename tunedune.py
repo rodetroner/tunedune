e41528e06f920635bd642bb21850df1db636e312
Szymon Hajnos <rodetroner@gmail.com>
2019-01-10 16:42:33 +0100

    Add docstrings

--- tunedune.py
+++ tunedune.py
@@ -8,9 +8,14 @@ kivy.require('1.10.1')
 Builder.load_file('sidebar.kv')
 
 class LoginScreen(Screen):
+
+    ''' Clear the login text field unless, the user typed their username '''
     def clearLogin(self, instance, *args):
         if self.login_input.text == 'Username':
             self.login_input.text = ''
+
+    ''' Clear the password text field unless, the user typed their username
+        and mask the input '''
     def clearPassword(self, instance, *args):
         if self.password_input.text == 'Password':
             self.password_input.text = ''
