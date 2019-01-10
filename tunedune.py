1e40f19848f941c56f7e01e881e91ed5c167b699
Szymon Hajnos <rodetroner@gmail.com>
2019-01-10 15:33:06 +0100

    Implement transition from login_screen to main_screen

--- tunedune.py
+++ tunedune.py
@@ -1,18 +1,25 @@
 import kivy
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 kivy.require('1.10.1')
 
 Builder.load_file('sidebar.kv')
 
-class MainLayout(BoxLayout):
+class LoginScreen(Screen):
     pass
+
+class MainScreen(Screen):
+    pass
+
 
 class TuneDuneApp(App):
     def build(self):
-        return MainLayout()
+        sm = ScreenManager()
+        sm.add_widget(LoginScreen(name='login_scr'))
+        sm.add_widget(MainScreen(name='main_scr'))
+        return sm
 
 if __name__ == '__main__':
     TuneDuneApp().run()
