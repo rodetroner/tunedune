6e485903ba88a64d4d4f0b2f1ed781ff57d97a4f
Szymon Hajnos <rodetroner@gmail.com>
2019-01-16 17:26:01 +0100

    Add placeholder search button

--- mainscreen.py
+++ mainscreen.py
@@ -1,7 +1,11 @@
 import kivy
 import sys
 sys.path.append('./model')
+sys.path.append('./mediaplayer_d')
+sys.path.append('./model/exceptions_d')
+sys.path.append('./model/user_d')
 import user_d.user
+import track_d.track
 from kivy.uix.screenmanager import Screen
 
 kivy.require('1.10.1')
@@ -10,3 +14,7 @@ class MainScreen(Screen):
     def logOut(self, instance, *args):
         user_d.user.User.logout(self.login_screen.current_session)
         print(self.login_screen.current_session)
+
+    def search(self, instance, *args):
+        track_d.track.search_track(name ='turystyczna_piosenka')
+        print(track_d.track.curr_searched_track_list)
